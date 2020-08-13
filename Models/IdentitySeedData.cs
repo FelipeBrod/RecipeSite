4a1765ce8d1a37d0a292d47d79d3f71cce5b6857
Felipe <Beraldo.felipe@gmail.com>
2020-08-13 11:30:00 -0400

    [ADD] User Controller

---
+++ Models/IdentitySeedData.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecipeSite.Models
+{
+    public class IdentitySeedData
+    {
+        private const string adminUser = "Admin";
+        private const string adminPassword = "Secret123$";
+
+        public static async void EnsurePopulated(IApplicationBuilder app)
+        {
+            UserManager<IdentityUser> userManager = app.ApplicationServices
+                .GetRequiredService<UserManager<IdentityUser>>();
+
+            IdentityUser user = await userManager.FindByIdAsync("Admin");
+
+            if (user == null)
+            {
+                user = new IdentityUser(adminUser);
+                await userManager.CreateAsync(user, adminPassword);
+            }
+
+        }
+    }
+}
